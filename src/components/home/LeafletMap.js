1a2330d2894d0da4d2c2477171fe3c11af4ff44e
unknown <nexog1997@gmail.com>
2020-08-31 19:30:18 +0200

    map bounds and zoom fix

--- src/components/home/LeafletMap.js
+++ src/components/home/LeafletMap.js
@@ -17,7 +17,16 @@ const LeafletMap = () => {
   }, []);
   console.log(markerData);
   return (
-    <Map className="leaflet-map" center={[52, 20]} zoom={5}>
+    <Map
+      className="leaflet-map"
+      center={[52, 20]}
+      zoom={5}
+      maxBounds={[
+        [90, -180],
+        [-90, 180],
+      ]}
+      minZoom={3}
+    >
       <TileLayer
         url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERNAME}/${MAP_STYLE}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAP_API_KEY}`}
         attribution="© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
